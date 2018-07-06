dacad75c4dcb1fc797b772e6e908d65fcab9b3cd
515074431 <515074431@qq.com>
2018-07-06 14:55:12 +0800

    暂时爬别人的试试

--- indexRecommendHuasheng.py
+++ indexRecommendHuasheng.py
@@ -8,13 +8,14 @@ import requests
 import json
 import time
 import urllib3
+import threading
 
 # 不显示错误
 urllib3.disable_warnings()
 from create_cache import create_cache
 
 
-def get_product_detail(goodsId):
+def get_product_detail(goodsId, tag):
     small_images_url = 'https://api.drgou.com/goods/undertakeGoods'
     detail_images_url = 'https://api.drgou.com/goods/goodsUrlList'
     small_images_result = requests.post(small_images_url, data={'goodsId': goodsId}, verify=False)
@@ -29,7 +30,7 @@ def get_product_detail(goodsId):
             create_cache('small_images_' + str(goodsId), small)
             create_cache('guess_like_' + str(goodsId), guess_like)
             create_cache('detail_images_' + str(goodsId), detail)
-            print('轮播图：%d 推荐关联： %d  详细图： %d' % (len(small), len(detail), len(guess_like)))
+            print('%s 轮播图：%d 推荐关联： %d  详细图： %d' % (tag, len(small), len(detail), len(guess_like)))
         else:
             if (not (small_images['status'] == 200)):
                 print('xxxxxxx轮播图推荐失败xxxxxx')
@@ -41,21 +42,35 @@ def get_product_detail(goodsId):
 def get_index_recommend_list():
     indexRecommendUrl = 'https://api.drgou.com/homgPage/guessLikeList'
     pageNo = 1
+
     while True:
         postData = {u'pageno': pageNo, u'pagesize': 20}
-        if(pageNo == 50):
+        if (pageNo == 50):
             break
         response = requests.post(indexRecommendUrl, data=postData, verify=False)
         if (response.status_code == 200):
             results = response.json()
             print('---------------%d---------------' % (pageNo))
             i = 0
-            for product in results['data']['guessLike']:
-                i = i + 1
-                print('%d: %s: %d' % (i, product['title'], product['goodsId']))
-                get_product_detail(product['goodsId'])
-            # 生成缓存
-            create_cache('indexRecommend_' + str(pageNo), results['data']['guessLike'])
+            threads = []
+            if(results['status'] == 200):
+                for product in results['data']['guessLike']:
+                    i = i + 1
+                    print('%d: %s: %d' % (i, product['title'], product['goodsId']))
+                    tag = '%d-%d ' % (pageNo, i)
+                    # get_product_detail(product['goodsId'])
+                    t = threading.Thread(target=get_product_detail, args=(product['goodsId'], tag))
+                    threads.append(t)
+                for thread in threads:
+                    thread.start()
+                for thread in threads:
+                    thread.join()
+
+                # 生成缓存
+                create_cache('indexRecommend_' + str(pageNo), results['data']['guessLike'])
+            else:
+                print('下载失败')
+                break
             if (i < 20):  # 没有时候退出
                 break
 
